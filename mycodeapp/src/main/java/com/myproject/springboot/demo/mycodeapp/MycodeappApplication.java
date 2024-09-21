8af25a32744eb7520011aafeb59117d3ec5999bb
Manas Patra <patramanas817@gmail.com>
2024-09-21 15:28:47 +0530

    first commit code

---
+++ mycodeapp/src/main/java/com/myproject/springboot/demo/mycodeapp/MycodeappApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.springboot.demo.mycodeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycodeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycodeappApplication.class, args);
+	}
+
+}
---
+++ mycodeapp/src/test/java/com/myproject/springboot/demo/mycodeapp/MycodeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.springboot.demo.mycodeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycodeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
