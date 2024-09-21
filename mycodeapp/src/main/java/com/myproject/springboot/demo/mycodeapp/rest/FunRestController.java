dbc02a3d0387b1c3306d42c5717446077707a301
Manas Patra <patramanas817@gmail.com>
2024-09-21 19:40:16 +0530

    learned injecting custom values in application properties file

--- mycodeapp/src/main/java/com/myproject/springboot/demo/mycodeapp/rest/FunRestController.java
+++ mycodeapp/src/main/java/com/myproject/springboot/demo/mycodeapp/rest/FunRestController.java
@@ -1,10 +1,24 @@
 package com.myproject.springboot.demo.mycodeapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // injecting properties for: coach name and team name
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamDetails(){
+        return "COACH NAME: " + coachName + " TEAM NAME: " + teamName;
+    }
+
     // expose '/' that returns "Hello World"
     @GetMapping("/")
     public String sayHello() {
