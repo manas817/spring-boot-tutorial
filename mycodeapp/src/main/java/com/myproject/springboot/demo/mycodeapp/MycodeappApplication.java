5ec559771f2f2a819db2c0fb067a5110a166f19f
Manas Patra <patramanas817@gmail.com>
2024-09-24 23:03:08 +0530

    implemented commandlinerunner and configured MySQL database

--- mycodeapp/src/main/java/com/myproject/springboot/demo/mycodeapp/MycodeappApplication.java
+++ mycodeapp/src/main/java/com/myproject/springboot/demo/mycodeapp/MycodeappApplication.java
@@ -1,13 +1,22 @@
 package com.myproject.springboot.demo.mycodeapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MycodeappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MycodeappApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("Hello World! ");
+		};
 	}
 
 }
