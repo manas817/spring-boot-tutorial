8b6130027de41fe80c96682109c776ee786cc161
Manas Patra <patramanas817@gmail.com>
2024-10-04 23:39:29 +0530

    Spring rest controller to get all students list

--- myapp/src/main/java/com/springboot/myapp/controller/DemoRestController.java
+++ myapp/src/main/java/com/springboot/myapp/controller/DemoRestController.java
@@ -1,15 +1,25 @@
 package com.springboot.myapp.controller;
 
+import com.springboot.myapp.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
-@RequestMapping("/home")
+@RequestMapping("/api")
 public class DemoRestController {
 
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello World!!";
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<>();
+
+        students.add(new Student("Manas", "Patra"));
+        students.add(new Student("Nishit", "Hingu"));
+        students.add(new Student("Gaurang", "Raje"));
+
+        return students;
     }
 }
