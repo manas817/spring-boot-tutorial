d150e68cfb2bd07b4bcb71a5ed0e3ada3d1e9615
Manas Patra <patramanas817@gmail.com>
2024-10-05 00:16:22 +0530

    rest controller - get student by id

--- myapp/src/main/java/com/springboot/myapp/controller/DemoRestController.java
+++ myapp/src/main/java/com/springboot/myapp/controller/DemoRestController.java
@@ -1,7 +1,9 @@
 package com.springboot.myapp.controller;
 
 import com.springboot.myapp.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,14 +14,25 @@ import java.util.List;
 @RequestMapping("/api")
 public class DemoRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
+    private List<Student> students;
+
+    // define post construct to load the student data .... only once!
+    @PostConstruct
+    public void loadData() {
+        students = new ArrayList<>();
 
         students.add(new Student("Manas", "Patra"));
-        students.add(new Student("Nishit", "Hingu"));
-        students.add(new Student("Gaurang", "Raje"));
+        students.add(new Student("sainikhil", "Pati"));
+        students.add(new Student("Varad", "Pai"));
+    }
 
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudentbyId(@PathVariable int studentId){
+        return students.get(studentId);
     }
 }
