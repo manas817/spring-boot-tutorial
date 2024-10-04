4ef71ddeccc9449f130c29162da05351249e211b
Manas Patra <patramanas817@gmail.com>
2024-10-05 00:57:39 +0530

    Added Exception for index out of bound in get request of student

--- myapp/src/main/java/com/springboot/myapp/controller/DemoRestController.java
+++ myapp/src/main/java/com/springboot/myapp/controller/DemoRestController.java
@@ -1,11 +1,11 @@
 package com.springboot.myapp.controller;
 
 import com.springboot.myapp.entity.Student;
+import com.springboot.myapp.entity.StudentErrorResponse;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,6 +33,20 @@ public class DemoRestController {
 
     @GetMapping("/students/{studentId}")
     public Student getStudentbyId(@PathVariable int studentId){
+
+        if (studentId >= students.size() || studentId < 0){
+            throw new StudentNotFoundException("Student id not found " + studentId);
+        }
         return students.get(studentId);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setMessage(exc.getMessage());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
