f6b655fb0ea41ee082c60ad539a665411dbce3a0
Manas Patra <patramanas817@gmail.com>
2024-10-06 15:53:17 +0530

    spring data rest: configs, pagination and sorting

--- myapp/src/main/java/com/springboot/myapp/repository/EmployeeRepository.java
+++ myapp/src/main/java/com/springboot/myapp/repository/EmployeeRepository.java
@@ -2,8 +2,10 @@ package com.springboot.myapp.repository;
 
 import com.springboot.myapp.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+// by default the endpoint is the plural form of entity name, in this case "employees" we can change it using RRR.
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
