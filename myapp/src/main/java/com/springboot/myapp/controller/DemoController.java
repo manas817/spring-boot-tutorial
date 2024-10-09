4581be8ac481021037deb2b001d39ffc0ad01042
Manas Patra <patramanas817@gmail.com>
2024-10-10 00:27:53 +0530

    thymeleaf data binding, display input form data in another web page

--- myapp/src/main/java/com/springboot/myapp/controller/DemoController.java
+++ myapp/src/main/java/com/springboot/myapp/controller/DemoController.java
@@ -3,14 +3,18 @@ package com.springboot.myapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
 public class DemoController {
 
-    @GetMapping("/hello")
-    public String sayHello(Model theModel) {
-        theModel.addAttribute("theDate", java.time.LocalDate.now());
+    @GetMapping("/showform")
+    public String showform() {
+        return "htmlform";
+    }
+    @GetMapping("/processform")
+    public String processform() {
         return "helloworld";
     }
 }
