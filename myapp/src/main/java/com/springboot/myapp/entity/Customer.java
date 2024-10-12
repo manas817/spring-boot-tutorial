41eb360a4768d273c51948bd8fc3efe607ea1b16
Manas Patra <patramanas817@gmail.com>
2024-10-12 19:42:19 +0530

    Spring Validation: added number range validation

--- myapp/src/main/java/com/springboot/myapp/entity/Customer.java
+++ myapp/src/main/java/com/springboot/myapp/entity/Customer.java
@@ -1,5 +1,6 @@
 package com.springboot.myapp.entity;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -10,6 +11,10 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min = 1, message = "is required")
     private String lastName;
+
+    @Min(value = 0, message = "value must be greater than zero")
+    @Max(value = 10, message = "value must be less than 10")
+    private String freePasses;
 
     public String getFirstName() {
         return firstName;
@@ -25,5 +30,13 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(String freePasses) {
+        this.freePasses = freePasses;
     }
 }
