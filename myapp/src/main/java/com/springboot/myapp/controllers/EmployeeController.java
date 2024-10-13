43469b944f888655acd095dded442f95a2f54742
Manas Patra <patramanas817@gmail.com>
2024-10-13 13:03:35 +0530

    spring MVC CRUD: added support for add employee

--- myapp/src/main/java/com/springboot/myapp/controllers/EmployeeController.java
+++ myapp/src/main/java/com/springboot/myapp/controllers/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -18,6 +20,18 @@ public class EmployeeController {
     @Autowired
     public EmployeeController(EmployeeService theEmployeeService){
         employeeService = theEmployeeService;
+    }
+
+    @GetMapping("/employeeAddForm")
+    public String employeeForm(Model theModel){
+        theModel.addAttribute("employee", new Employee());
+        return "employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee){
+        employeeService.save(theEmployee);
+        return "redirect:/employees/list";
     }
 
     @GetMapping("/list")
