c735a6d1ed179fe355629e84bc3d3966d13c735e
Manas Patra <patramanas817@gmail.com>
2024-10-13 11:04:26 +0530

    spring validation: custom validation annotation

--- myapp/src/main/java/com/springboot/myapp/entity/Customer.java
+++ myapp/src/main/java/com/springboot/myapp/entity/Customer.java
@@ -1,5 +1,6 @@
 package com.springboot.myapp.entity;
 
+import com.springboot.myapp.validation.CourseCode;
 import jakarta.validation.constraints.*;
 
 public class Customer {
@@ -16,6 +17,9 @@ public class Customer {
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
     private String postalCode;
+
+    @CourseCode(value = "DIP", message = "must start with DIP")
+    private String courseCode;
 
     public String getFirstName() {
         return firstName;
@@ -47,5 +51,13 @@ public class Customer {
 
     public void setPostalCode(String postalCode) {
         this.postalCode = postalCode;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
     }
 }
