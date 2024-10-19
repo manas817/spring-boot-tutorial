94e8a71f1fc036b4d545fc9b3e2973045e5fd1b0
Manas Patra <patramanas817@gmail.com>
2024-10-19 12:47:35 +0530

    JPA/Hibernate: OnetoMany association between instructor and Course, added entities

--- myapp/src/main/java/com/springboot/myapp/entity/Instructor.java
+++ myapp/src/main/java/com/springboot/myapp/entity/Instructor.java
@@ -2,6 +2,9 @@ package com.springboot.myapp.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "instructor")
 public class Instructor {
@@ -23,6 +26,10 @@ public class Instructor {
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "instructor_detail_id")
     private InstructorDetail instructorDetail;
+
+    @OneToMany(mappedBy = "instructor",
+            cascade = {CascadeType.MERGE, CascadeType.DETACH, CascadeType.PERSIST, CascadeType.REFRESH})
+    private List<Course> courses;
 
     // getters and setters
 
@@ -66,6 +73,14 @@ public class Instructor {
         this.id = id;
     }
 
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
     // constructor
     public Instructor() {
 
@@ -86,5 +101,13 @@ public class Instructor {
                 ", email='" + email + '\'' +
                 ", instructorDetail=" + instructorDetail +
                 '}';
+    }
+
+    public void add(Course tempCourse) {
+        if(courses == null) {
+            courses = new ArrayList<>();
+        }
+        courses.add(tempCourse);
+        tempCourse.setInstructor(this);
     }
 }
